247d1515943caf1ced5727cbd7c17faba197ca34
Alexander Senchenko <alexander.p.senchenko@gmail.com>
2023-03-20 22:34:46 +0700

    Init test gradle

---
+++ TestGradle/app/src/main/java/ru/eltex/User.java
@@ -0,0 +1,38 @@
+package ru.eltex;
+
+public class User {
+
+    Integer id;
+    String name;
+
+    public User() {}
+
+    User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
