8b5fc88e089d42bd8a430b65c525f666eaf46f9f
Alexander Senchenko <alexander.p.senchenko@gmail.com>
2023-03-23 17:48:44 +0700

    Add edit text for Developer and Manager

--- TestList/app/src/main/java/com/example/eltex/testlist/AddActivity.java
+++ TestList/app/src/main/java/com/example/eltex/testlist/AddActivity.java
@@ -5,8 +5,10 @@ import static com.example.eltex.testlist.MainActivity.users;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -20,6 +22,26 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
+        ((RadioGroup) findViewById(R.id.radio_group)).setOnCheckedChangeListener((radioGroup, checkedId) -> {
+            int visibilityDeveloperBlock = View.INVISIBLE;
+            int visibilityManagerBlock = View.INVISIBLE;
+
+            switch (checkedId) {
+                case R.id.button_developer:
+                    visibilityDeveloperBlock = View.VISIBLE;
+                    visibilityManagerBlock = View.INVISIBLE;
+                    break;
+
+                case R.id.button_manager:
+                    visibilityDeveloperBlock = View.INVISIBLE;
+                    visibilityManagerBlock = View.VISIBLE;
+                    break;
+            }
+
+            ((LinearLayout) findViewById(R.id.languages_block)).setVisibility(visibilityDeveloperBlock);
+            ((LinearLayout) findViewById(R.id.count_projects_block)).setVisibility(visibilityManagerBlock);
+        });
+
         ((Button) findViewById(R.id.add_button)).setOnClickListener(view -> {
             int checkedRadioButtonId = ((RadioGroup) findViewById(R.id.radio_group)).getCheckedRadioButtonId();
             String name = ((EditText) findViewById(R.id.name)).getText().toString();
@@ -32,15 +54,19 @@ public class AddActivity extends AppCompatActivity {
 
             switch (checkedRadioButtonId) {
                 case R.id.button_developer:
-                    MainActivity:users.add(new Developer(name, phone, List.of("")));
+                    String languages = ((EditText) findViewById(R.id.languages)).getText().toString();
+
+                    MainActivity:users.add(new Developer(name, phone, List.of(languages)));
                     break;
 
                 case R.id.button_manager:
-                    MainActivity:users.add(new Manager(name, phone, 0));
+                    String countProjects = ((EditText) findViewById(R.id.count_projects)).getText().toString();
+
+                    MainActivity:users.add(new Manager(name, phone, Integer.valueOf(countProjects)));
                     break;
             }
 
-            System.out.println(name + " " + phone);
+//            System.out.println(users.get(users.size() - 1).toString());
 
             finish();
         });
